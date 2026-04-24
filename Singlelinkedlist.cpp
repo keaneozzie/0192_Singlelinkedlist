000e30c65b5c3d7ad28a2867bc4d5fc3a7fd06bc
keaneozzie <ozziedharma897@gmail.com>
2026-04-24 18:39:52 +0700

    implement fungsi addnode

--- Singlelinkedlist.cpp
+++ Singlelinkedlist.cpp
@@ -18,3 +18,13 @@ Linkedlist()
     START = NULL;
 }
 
+void addNode()
+{
+    int nim;
+    cout << "\nMasukkan Nomor Mahasiswa: ";
+    cin >> nim;
+
+    Node *nodeBaru = new Node;
+    nodeBaru->noMhs = nim;
+
+    
